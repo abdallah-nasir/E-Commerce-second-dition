a069769023dfd38f80a74acd6e7f50806fd5335a
abdallah-nasir <abdallah.nasir@Ymail.com>
2021-09-06 01:33:33 +0200

    deploy-35

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^yvdhx^w5$jah)-p!vbbqy5vf9qco&ge)p0m^nr_p%$=4&6i56'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #TEMPLATE_DEBUG = DEBUG
 if DEBUG == False:
     TEMPLATE_DEBUG = DEBUG
@@ -218,23 +218,23 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {    
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES={
-#     "default":{
-#         "ENGINE":"django.db.backends.postgresql_psycopg2",
-#         "NAME":"deqdvj9oqtl78t",
-#         "USER":"yiqxgupmlcyarh",
-#         "PASSWORD":"27c973e6ad81c95804856b99446bf8f93ddecb2919583f52691056bca5cffa76",
-#         "HOST":"ec2-34-194-130-103.compute-1.amazonaws.com",
-#         "PORT":"5432"
-#     }     
+# DATABASES = {    
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES={
+    "default":{
+        "ENGINE":"django.db.backends.postgresql_psycopg2",
+        "NAME":"deqdvj9oqtl78t",
+        "USER":"yiqxgupmlcyarh",
+        "PASSWORD":"27c973e6ad81c95804856b99446bf8f93ddecb2919583f52691056bca5cffa76",
+        "HOST":"ec2-34-194-130-103.compute-1.amazonaws.com",
+        "PORT":"5432"
+    }     
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
     
